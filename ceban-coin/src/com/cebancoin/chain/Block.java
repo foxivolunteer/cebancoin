c8299c432ab744458847108e2086dc2e0129c12b
foxi <foxi@192.168.1.4>
2018-06-14 17:09:15 +0300

    Wallet added. key pair added.

--- ceban-coin/src/com/cebancoin/chain/Block.java
+++ ceban-coin/src/com/cebancoin/chain/Block.java
@@ -1,8 +1,10 @@
 package com.cebancoin.chain;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 import com.cebancoin.crypto.DigitalSignature;
+import com.cebancoin.service.Transaction;
  
 public class Block {
 	 
@@ -15,51 +17,52 @@ public class Block {
 	//time in milliseconds
 	private long timeStamp;
 	private int nonce;
+	public String merkleRoot;
+	//our data will be a simple message.
+	public ArrayList<Transaction> transactions = new ArrayList<Transaction>(); 
 	
-	public Block(String hash, String previousHash) {
-		super();
-		this.hash = calculateHash();
+	//Block Constructor.  
+	public Block(String previousHash ) {
 		this.previousHash = previousHash;
-		this.setData(data);
-		this.setTimeStamp(new Date().getTime());
+		this.timeStamp = new Date().getTime();
+		
+		this.hash = calculateHash(); //Making sure we do this after we set the other values.
 	}
 	
+	//Calculate new hash based on blocks contents
 	public String calculateHash() {
-		String applyText = previousHash + Long.toString(timeStamp) + data;
-		String calculatedhash = DigitalSignature.encyriptSha256(applyText);
+		String calculatedhash = DigitalSignature.encyriptSha256( 
+				previousHash +
+				Long.toString(timeStamp) +
+				Integer.toString(nonce) + 
+				merkleRoot
+				);
 		return calculatedhash;
 	}
 	
+	//Increases nonce value until hash target is reached.
 	public void mineBlock(int difficulty) {
-		String target = new String(new char[difficulty]).replace('\0', '0'); //Create a string with difficulty * "0" 
+		merkleRoot = DigitalSignature.getMerkleRoot(transactions);
+		String target = DigitalSignature.getDificultyString(difficulty); //Create a string with difficulty * "0" 
 		while(!hash.substring( 0, difficulty).equals(target)) {
-			setNonce(getNonce() + 1);
+			nonce ++;
 			hash = calculateHash();
 		}
 		System.out.println("Block Mined!!! : " + hash);
+	}
+	
+	//Add transactions to this block
+	public boolean addTransaction(Transaction transaction) {
+		//process transaction and check if valid, unless block is genesis block then ignore.
+		if(transaction == null) return false;		
+		if((previousHash != "0")) {
+			if((transaction.processTransaction() != true)) {
+				System.out.println("Transaction failed to process. Discarded.");
+				return false;
+			}
+		}
+		transactions.add(transaction);
+		System.out.println("Transaction Successfully added to Block");
+		return true;
 }
-
-	public String getData() {
-		return data;
-	}
-
-	public void setData(String data) {
-		this.data = data;
-	}
-
-	public long getTimeStamp() {
-		return timeStamp;
-	}
-
-	public void setTimeStamp(long timeStamp) {
-		this.timeStamp = timeStamp;
-	}
-
-	public int getNonce() {
-		return nonce;
-	}
-
-	public void setNonce(int nonce) {
-		this.nonce = nonce;
-	}
 }
---
+++ ceban-coin/src/com/cebancoin/service/TransactionInput.java
@@ -0,0 +1,12 @@
+package com.cebancoin.service;
+
+public class TransactionInput {
+	// Reference to TransactionOutputs -> transactionId
+	public String transactionOutputId;
+	// Contains the Unspent transaction output
+	public TransactionOutput UTXO;
+
+	public TransactionInput(String transactionOutputId) {
+		this.transactionOutputId = transactionOutputId;
+	}
+}
